c839ab583137ef1318dde3faeb7b0de85d2b6eb6
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2026-06-08 10:40:32 -0500

    feature: Add SEO with sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,18 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
+  site: "https://www.onepiece.dejeloper.com",
   prefetch: {
     prefetchAll: true,
     defaultStrategy: "viewport",
   },
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
